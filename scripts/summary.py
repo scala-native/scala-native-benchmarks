dda3461a90f256c02d12d005c77d55cd67e35788
Valdis <valdisxp1@gmail.com>
2018-09-23 13:10:40 +0200

    save charts to reports

--- scripts/summary.py
+++ scripts/summary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
-from run import benchmarks, runs, configurations
+from run import benchmarks, runs, configurations, mkdir
 
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+
 
 def config_data(bench, conf):
     out = []
@@ -24,39 +25,79 @@ def config_data(bench, conf):
             pass
     return np.array(out)
 
-def peak_performance():
+
+def hot_config_data(bench, conf):
+    out = []
+    for run in xrange(runs):
+        try:
+            points = []
+            with open('results/{}/{}/{}'.format(conf, bench, run)) as data:
+                for line in data.readlines():
+                    points.append(float(line))
+            # take only last 1000 to account for startup
+            out += points[-1000:]
+        except IOError:
+            pass
+    return np.array(out)
+
+
+def peak_performance(percentile):
     out = []
     for bench in benchmarks:
         res = []
         for conf in configurations:
             try:
-                res.append(np.percentile(config_data(bench, conf), 50))
+                res.append(np.percentile(config_data(bench, conf), percentile))
             except IndexError:
                 res.append(0)
         out.append(res)
     return out
 
-def p50_chart(plt):
+
+# not good
+def bar_chart(plt, percentile):
     ind = np.arange(len(benchmarks))
     for conf in configurations:
         res = []
         for bench in benchmarks:
             try:
-                res.append(np.percentile(config_data(bench, conf), 50))
+                res.append(np.percentile(config_data(bench, conf), percentile))
             except IndexError:
                 res.append(0)
         plt.bar(ind, res, align='center', label=conf)
-    plt.xticks(ind, map(lambda x: x.split(".")[0],benchmarks))
+    plt.xticks(ind, map(benchmark_short_name, benchmarks))
     plt.legend()
-    plt.show()
+    return plt
 
 
-if __name__ == '__main__':
+def percentiles_chart(plt, bench, limit=99):
+    for conf in configurations:
+        data = hot_config_data(bench, conf)
+        percentiles = np.arange(0, limit)
+        percvalue = np.array([np.percentile(data, perc) for perc in percentiles])
+        plt.plot(percentiles, percvalue, label = conf)
+    plt.legend()
+    plt.title(bench)
+    return plt
+
+def print_table(data):
     leading = ['name']
     for conf in configurations:
         leading.append(conf)
     print ','.join(leading)
-    for bench, res in zip(benchmarks, peak_performance()):
-        print ','.join([bench.split(".")[0]] + list(map(str, res)))
-    p50_chart(plt)
+    for bench, res in zip(benchmarks, data):
+        print ','.join([benchmark_short_name(bench)] + list(map(str, res)))
 
+
+def benchmark_short_name(bench):
+    return bench.split(".")[0]
+
+
+if __name__ == '__main__':
+    print_table(peak_performance(50))
+    # bar_chart(plt, 50).show()
+    mkdir("reports")
+    for bench in benchmarks:
+        percentiles_chart(plt, bench).savefig("reports/percentile_" + bench + ".png")
+        plt.clf()
+        plt.cla()
