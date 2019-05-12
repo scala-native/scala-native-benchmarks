c724809de49ec80d0c829d55d2e3d9556232025b
Valdis <valdisxp1@gmail.com>
2019-05-12 23:09:12 +0200

    autodiscover all configs add all the latest configs up to 0.4.0-SNAPSHOT

--- scripts/run.py
+++ scripts/run.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-import sys
 import os
 import errno
 import subprocess as subp
 import shutil as sh
+import argparse
+
 
 def mkdir(path):
     try:
@@ -14,9 +15,11 @@ def mkdir(path):
         else:
             raise
 
+
 def slurp(path):
     with open(path) as f:
         return f.read().strip()
+
 
 def where(cmd):
     if os.path.isfile(cmd):
@@ -30,15 +33,18 @@ def where(cmd):
         else:
             return None
 
+
 def run(cmd):
     print(">>> " + str(cmd))
     return subp.check_output(cmd)
+
 
 def compile(bench, compilecmd):
     cmd = [sbt, '-J-Xmx6G', 'clean']
     cmd.append('set mainClass in Compile := Some("{}")'.format(bench))
     cmd.append(compilecmd)
     return run(cmd)
+
 
 sbt = where('sbt')
 
@@ -61,15 +67,15 @@ benchmarks = [
     'mandelbrot.MandelbrotBenchmark',
 ]
 
-configurations = [
-        # 'jvm',
-        # 'scala-native-0.3.8',
-        # 'baseline',
-        'current'
-]
+stable = 'scala-native-0.3.9'
+latest = 'scala-native-0.4.0-SNAPSHOT'
+
+confs_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/confs"
+
+all_configs = next(os.walk(confs_path))[1]
 
 if 'GRAALVM_HOME' in os.environ:
-    configurations += [
+    all_configs += [
             'native-image',
             'native-image-pgo',
     ]
@@ -78,7 +84,34 @@ runs = 20
 batches = 4000
 batch_size = 1
 
+
+def expand_wild_cards(arg):
+    if arg == None:
+        return arg
+    elif arg.startswith("latest"):
+        return latest + arg[len("latest"):]
+    elif arg.startswith("stable"):
+        return stable + arg[len("stable"):]
+    else:
+        return arg
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("set", nargs='*', default=[None])
+    args = parser.parse_args()
+    print args
+
+    configurations = []
+    for choice in args.set:
+        expanded = expand_wild_cards(choice)
+        if expanded == None:
+            configurations = [stable, latest]
+        else:
+            configurations += [expanded]
+
+    print "configurations:", configurations
+
     for conf in configurations:
         for bench in benchmarks:
             print('--- conf: {}, bench: {}'.format(conf, bench))
