9a5559d9807b7d11aee699f7ed1dd316232f82e5
Valdis <valdisxp1@gmail.com>
2019-05-18 09:50:57 +0200

    persist settings in the results folders

--- scripts/shared/configurations.py
+++ scripts/shared/configurations.py
@@ -5,6 +5,7 @@ from benchmarks import Benchmark
 from file_utils import slurp, mkdir
 
 confs_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/confs"
+results_path = 'results'
 
 default_batches = 4000
 default_runs = 20
@@ -25,8 +26,32 @@ class Configuration:
         if batches != default_batches:
             suffix += "-b" + str(batches)
 
-        self.results_dir = os.path.join('results', self.name + suffix)
+        self.results_dir = os.path.join(results_path, self.name + suffix)
         print self.conf_dir
+
+    @classmethod
+    def from_results(cls, full_name):
+        return cls.from_results_dir(os.path.join(results_path, full_name))
+
+    @classmethod
+    def from_results_dir(cls, full_path):
+        name = None
+        batches = None
+        runs = None
+        with open(os.path.join(full_path, 'settings.properties')) as settings:
+            for line in settings.readlines():
+                key, value = line.split('=')
+                if key == 'name':
+                    name = value
+                elif key == 'batches':
+                    batches = value
+                elif key == 'runs':
+                    runs = value
+        if batches is None:
+            batches = default_batches
+        if runs is None:
+            runs = default_runs
+        return cls(name, batches=batches, runs=runs)
 
     def make_active(self):
         build_sbt_src = os.path.join(self.conf_dir, 'build.sbt')
@@ -51,14 +76,14 @@ class Configuration:
             print('--- conf: {}, bench: {}'.format(self.name, bench))
 
             bench.compile(self)
-            resultsdir = bench.ensure_results_dir(self)
+            results_dir = bench.ensure_results_dir(self)
 
             runs = self.runs
             for n in xrange(runs):
                 print('--- run {}/{}'.format(n, runs))
 
                 out = bench.run(self)
-                with open(os.path.join(resultsdir, str(n)), 'w+') as resultfile:
+                with open(os.path.join(results_dir, str(n)), 'w+') as resultfile:
                     resultfile.write(out)
 
     def run_cmd(self, bench):
@@ -69,6 +94,13 @@ class Configuration:
 
     def compile_cmd(self):
         return slurp(os.path.join(self.conf_dir, 'compile'))
+
+    def write_settings(self):
+        results_dir = self.results_dir
+        with open(os.path.join(results_dir, 'settings.properties'), 'w+') as settings:
+            settings.write('name={}\n'.format(self.name))
+            settings.write('batches={}\n'.format(self.batches))
+            settings.write('runs={}\n'.format(self.runs))
 
     def ensure_results_dir(self):
         dir = self.results_dir
